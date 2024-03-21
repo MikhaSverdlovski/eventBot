aa36f661fcab249f55b327f492f2a84170c93ca7
MikhaSverdlovski <shirinkin.mikhail1992@gmail.com>
2024-03-21 09:22:24 +0300

    logging added

--- Security.py
+++ Security.py
@@ -1,5 +1,7 @@
-
+import asyncio
 import functools
+
+import aiofiles
 from aiogram import types
 
 from Config import Config
@@ -14,6 +16,15 @@ async def get_chat(message: types.Message):
     return my_id
 
 
+async def get_user(message: types.Message):
+    try:
+        username = message.from_user.username
+        return username
+    except Exception as e:
+        print("Error:", e)
+        return None
+
+
 def check_user(func):
     @functools.wraps(func)
     async def wrapper(*args, **kwargs):
@@ -23,10 +34,19 @@ def check_user(func):
             mychat_id = await get_chat(message)
             conf = Config.Chat_id
             if str(mychat_id) == conf:
+                username = await get_user(message)
+                await logging(chat_id=mychat_id, username=username)
                 return await func(*args, **kwargs)
             else:
+                await logging(chat_id=mychat_id, username=get_user(message))
                 return await message.answer("Свяжитесь с Михаилом Свердловским. Приложение только для него")
         else:
             raise ValueError("Argument 'message' is required for check_user decorator")
 
     return wrapper
+
+
+async def logging(chat_id: str, username: str) -> None:
+    if username is not None:
+        async with aiofiles.open("log.txt", "a") as f:
+            await f.write(f"chat_id: {chat_id} Username: {username}\n")
