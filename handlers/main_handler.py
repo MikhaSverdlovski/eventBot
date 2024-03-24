425518f8f66c54e2accc7a29acba2dbe7a6fd5a8
MikhaSverdlovski <shirinkin.mikhail1992@gmail.com>
2024-03-24 12:30:31 +0300

    registration added States

--- handlers/main_handler.py
+++ handlers/main_handler.py
@@ -1,6 +1,13 @@
+from aiogram.filters import StateFilter
+from aiogram.fsm.state import default_state
 from aiogram.types import FSInputFile
 from aiogram import types, Router
-from aiogram.filters.command import Command
+from aiogram import F, Router
+from aiogram.filters import Command
+from aiogram.filters import StateFilter
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import default_state
+from aiogram.types import Message, ReplyKeyboardRemove
 
 
 router = Router()
@@ -9,10 +16,11 @@ router = Router()
 async def cmd_start(message: types.Message):
     photo = FSInputFile("Static/1.jpg")
     caption = (f"<B>Добрый день, {message.from_user.first_name}</b> \n\n"
-               "Здесь вы можете записаться на ближайшее мероприятие\n\n"
+               "Здесь Вы можете записаться на ближайшее мероприятие\n\n"
                "Cписок команд:\n"
                "\n"
                "/start - Перезапустить бота\n"
                "/register - Пройти процедуру регистрации\n"
                "/help - Помощь")
     await message.answer_photo(photo, caption=caption)
+
--- handlers/registration_handler.py
+++ handlers/registration_handler.py
@@ -1,29 +1,116 @@
-from aiogram import Bot, Dispatcher, types, F, Router
-from aiogram.filters.command import Command
+from aiogram import Router, F, types
+from aiogram.filters import Command, StateFilter
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import Message, ReplyKeyboardRemove
+from aiogram.fsm.state import default_state
 
 router = Router()
 
+# Эти значения далее будут подставляться в итоговый текст, отсюда
+# такая на первый взгляд странная форма прилагательных
+available_food_sizes = ["Маленькую", "Среднюю", "Большую"]
+available_food_names = ["Суши", "Спагетти", "Хачапури"]
 
-@router.message(Command("register"))
-async def cmd_register(message: types.Message):
-    kb = [
-        [
-            types.KeyboardButton(text="С пюрешкой"),
-            types.KeyboardButton(text="Без пюрешки")
-        ],
-    ]
+kb_avail_food_names = [
+    [types.KeyboardButton(text=size)] for size in available_food_names
+]
+
+kb_avail_food_sizes = [
+    [types.KeyboardButton(text=size)] for size in available_food_sizes
+]
+
+
+class OrderFood(StatesGroup):
+    choosing_food_name = State()
+    choosing_food_size = State()
+
+
+@router.message(Command("food"))
+async def cmd_food(message: Message, state: FSMContext):
     keyboard = types.ReplyKeyboardMarkup(
-        keyboard=kb,
+        keyboard=kb_avail_food_names,
         resize_keyboard=True,
-        input_field_placeholder="Выберите способ подачи"
+        input_field_placeholder="Выберите блюдо"
     )
-    await message.answer("Как подавать котлеты?", reply_markup=keyboard)
+    await message.answer(
+        text="Выберите блюдо:",
+        reply_markup=keyboard
+    )
+    # Устанавливаем пользователю состояние "выбирает название"
+    await state.set_state(OrderFood.choosing_food_name)
 
 
-@router.message(F.text.lower() == "с пюрешкой")
-async def with_puree(message: types.Message):
-    await message.reply("Отличный выбор!", reply_markup=types.ReplyKeyboardRemove())
+@router.message(OrderFood.choosing_food_name, F.text.in_(available_food_names))
+async def food_chosen(message: Message, state: FSMContext):
+    await state.update_data(chosen_food=message.text.lower())
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb_avail_food_sizes,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите блюдо"
+    )
+    await message.answer(
+        text="Спасибо. Теперь, пожалуйста, выберите размер порции:",
+        reply_markup=keyboard
+    )
+    await state.set_state(OrderFood.choosing_food_size)
 
-@router.message(F.text.lower() == "без пюрешки")
-async def without_puree(message: types.Message):
-    await message.reply("Так невкусно!", reply_markup=types.ReplyKeyboardRemove())
+
+@router.message(StateFilter("OrderFood:choosing_food_name"))
+async def food_chosen_incorrectly(message: Message):
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb_avail_food_names,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите блюдо"
+    )
+    await message.answer(
+        text="Я не знаю такого блюда.\n\n"
+             "Пожалуйста, выберите одно из названий из списка ниже:",
+        reply_markup=keyboard
+    )
+
+
+@router.message(OrderFood.choosing_food_size, F.text.in_(available_food_sizes))
+async def food_size_chosen(message: Message, state: FSMContext):
+    user_data = await state.get_data()
+    await message.answer(
+        text=f"Вы выбрали {message.text.lower()} порцию {user_data['chosen_food']}.\n"
+             f"Попробуйте теперь заказать напитки: /drinks",
+        reply_markup=ReplyKeyboardRemove()
+    )
+    # Сброс состояния и сохранённых данных у пользователя
+    await state.clear()
+
+
+@router.message(OrderFood.choosing_food_size)
+async def food_size_chosen_incorrectly(message: Message):
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb_avail_food_sizes,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите блюдо"
+    )
+    await message.answer(
+        text="Я не знаю такого размера порции.\n\n"
+             "Пожалуйста, выберите один из вариантов из списка ниже:",
+        reply_markup=keyboard
+    )
+
+@router.message(StateFilter(None), Command(commands=["cancel"]))
+@router.message(default_state, F.text.lower() == "отмена")
+async def cmd_cancel_no_state(message: Message, state: FSMContext):
+    # Стейт сбрасывать не нужно, удалим только данные
+    await state.set_data({})
+    await message.answer(
+        text="Нечего отменять",
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+
+@router.message(Command(commands=["cancel"]))
+@router.message(F.text.lower() == "отмена")
+async def cmd_cancel(message: Message, state: FSMContext):
+    await state.clear()
+    await message.answer(
+        text="Действие отменено",
+        reply_markup=ReplyKeyboardRemove()
+    )
